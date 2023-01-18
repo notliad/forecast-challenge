175468c33383b4d5dc81dc8de28b8e20b7729336
notliad <dailton.rcf@gmail.com>
2023-01-18 00:01:17 -0300

    feat: create reverseGeocode function

---
+++ src/services/OpenCage.js
@@ -0,0 +1,13 @@
+export async function reverseGeocode(lat, long) {
+  if (lat && long) {
+    try {
+      const response = await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${lat},${long}&key=c63386b4f77e46de817bdf94f552cddf&language=en`
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
