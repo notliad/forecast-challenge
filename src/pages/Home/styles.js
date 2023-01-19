3c72a0d98c594a84ff742f49350ad1e37ccdd555
notliad <dailton.rcf@gmail.com>
2023-01-18 23:23:04 -0300

    draft style to home page

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,32 +1,17 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  width: 100%;
-`;
+export const Container = styled.div``;
 
-export const HeaderContent = styled.header`
+export const ComponentsContainer = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 
   padding: 5rem 5rem;
 
-  background-color: #f333;
-
-  > div {
-    width: 110rem;
-  }
-
-  @media (max-width: 1200px) {
-    > div {
-      width: 100%;
-      margin: 0rem 3rem;
-    }
-  }
-
-  @media (max-width: 500px) {
-    padding: 2rem;
-  }
+  background-color: #fff;
 `;
 
 export const MainContent = styled.main`
