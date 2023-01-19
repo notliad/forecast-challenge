a56e72ed60e3cef8c7c2d612e33ed057ac1a2968
notliad <dailton.rcf@gmail.com>
2023-01-18 23:23:51 -0300

    new api key

--- src/services/OpenWeather.js
+++ src/services/OpenWeather.js
@@ -2,7 +2,7 @@ export async function getWeather(locationName) {
   if (locationName) {
     try {
       const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=772920597e4ec8f00de8d376dfb3f094&units=metric`
+        `http://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=da48942f2ced9fddfad80cd983aa7a20&units=metric`
       );
       const data = await response.json();
       return data;
