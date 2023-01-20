fd07b691e5b642d1a61f04962e76fd20ec4cc693
notliad <dailton.rcf@gmail.com>
2023-01-20 00:15:00 -0300

    style of input

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,25 +1,26 @@
 import styled from "styled-components";
-import { ReactComponent as InputLogo } from "../../assets/icons/44.svg";
+import { ReactComponent as InputLogo } from "../../assets/icons/searchIcon.svg";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
   background-color: #efeae7;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Form = styled.form``;
 
 export const Input = styled.input`
-  width: 100%;
   padding: 6px 10px;
   margin: 10px 0;
+  border: none;
   background-color: #efeae7;
-  color: #7c7877;
+  outline: none;
+  font-size: 18px;
 `;
 
 export const SearchIcon = styled(InputLogo)`
   width: 25px;
   height: 25px;
-  align-items: center;
-  justify-content: center;
 `;
