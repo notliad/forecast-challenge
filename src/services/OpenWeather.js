64b07f99cdd3f234672e809ce9d39d8f705f5fee
notliad <dailton.rcf@gmail.com>
2023-01-20 00:12:39 -0300

    fix fetch url to get 3 days of forecast

--- src/services/OpenWeather.js
+++ src/services/OpenWeather.js
@@ -2,10 +2,13 @@ export async function getWeather(locationName) {
   if (locationName) {
     try {
       const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=da48942f2ced9fddfad80cd983aa7a20&units=metric`
+        `http://api.openweathermap.org/data/2.5/forecast?q=${locationName}&appid=da48942f2ced9fddfad80cd983aa7a20&units=metric`
       );
       const data = await response.json();
-      return data;
+      return {
+        status: data.cod,
+        data: [data.list[0], data.list[8], data.list[16]],
+      };
     } catch (error) {
       console.error(error);
     }
