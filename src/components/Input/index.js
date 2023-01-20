e83db35856e2f4b7029d775e4b10ae31656a717c
notliad <dailton.rcf@gmail.com>
2023-01-20 00:14:47 -0300

    first logic to dashboard

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,14 +4,18 @@ import * as S from "./styles";
 const Input = ({ input, setInput, setLocation }) => {
   return (
     <S.Container>
-      <S.SearchIcon color={"#7c7877"} src="../../assets/icons/44.svg" />
-      <S.Form onSubmit={() => setLocation(input)}>
+      <S.SearchIcon color={"#7c7877"} />
+      <S.Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setLocation(input);
+        }}
+      >
         <S.Input
           type="text"
           value={input}
           onChange={(input) => setInput(input.target.value)}
         />
-        <S.Input type="submit" />
       </S.Form>
     </S.Container>
   );
