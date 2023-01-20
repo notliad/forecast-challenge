d47cef90844fd8592aefaf3f5fd5ca04d5a0c399
notliad <dailton.rcf@gmail.com>
2023-01-20 00:15:51 -0300

    remove unused style and apply final style to home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,20 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div``;
 
 export const ComponentsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
+  margin: 0 20%;
+  height: 100vh;
   align-items: center;
   justify-content: center;
-
-  padding: 5rem 5rem;
-
-  background-color: #fff;
-`;
-
-export const MainContent = styled.main`
-  margin-top: 5rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  background-color: #ff33;
 `;
