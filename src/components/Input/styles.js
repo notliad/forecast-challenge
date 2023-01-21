dd1bbfb64836c467469000c63a15319ca320d132
notliad <dailton.rcf@gmail.com>
2023-01-21 03:14:54 -0300

    adjusting margins and visuals for input component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,16 +2,22 @@ import styled from "styled-components";
 import { ReactComponent as InputLogo } from "../../assets/icons/searchIcon.svg";
 
 export const Container = styled.div`
+  width: 10 0%;
   display: flex;
   flex-direction: row;
   background-color: #efeae7;
   align-items: center;
   justify-content: center;
+  padding-left: 10px;
 `;
 
-export const Form = styled.form``;
+export const Form = styled.form`
+  width: 100%;
+`;
 
 export const Input = styled.input`
+  max-width: 350px;
+  width: 100%;
   padding: 6px 10px;
   margin: 10px 0;
   border: none;
@@ -23,4 +29,8 @@ export const Input = styled.input`
 export const SearchIcon = styled(InputLogo)`
   width: 25px;
   height: 25px;
+`;
+
+export const FormContainer = styled.div`
+  width: 100%;
 `;
