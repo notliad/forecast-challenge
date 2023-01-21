2d9deafa1a1b626c3622a02e7fd9e01cb8542b4a
notliad <dailton.rcf@gmail.com>
2023-01-21 03:14:30 -0300

    add reference to input control and remove unused code

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,22 +1,24 @@
-import { useEffect, useState } from "react";
 import * as S from "./styles";
 
-const Input = ({ input, setInput, setLocation }) => {
+const Input = ({ input, setInput, setLocation, reference }) => {
   return (
     <S.Container>
       <S.SearchIcon color={"#7c7877"} />
-      <S.Form
-        onSubmit={(event) => {
-          event.preventDefault();
-          setLocation(input);
-        }}
-      >
-        <S.Input
-          type="text"
-          value={input}
-          onChange={(input) => setInput(input.target.value)}
-        />
-      </S.Form>
+      <S.FormContainer>
+        <S.Form
+          onSubmit={(event) => {
+            event.preventDefault();
+            setLocation(input);
+          }}
+        >
+          <S.Input
+            type="text"
+            value={input}
+            onChange={(input) => setInput(input.target.value)}
+            ref={reference}
+          />
+        </S.Form>
+      </S.FormContainer>
     </S.Container>
   );
 };
