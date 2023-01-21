4f415811b6b8d869ce092f69c7f44e40e3afe6b8
notliad <dailton.rcf@gmail.com>
2023-01-21 03:12:39 -0300

    add today and forecast component to components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Input from "./Input";
 import Dashboard from "./Dashboard";
+import Today from "./Today";
+import Forecast from "./Forecast";
 
-export { Input, Dashboard };
+export { Input, Dashboard, Today, Forecast };
