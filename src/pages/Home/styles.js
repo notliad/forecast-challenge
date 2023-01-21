445b3696b3b081f2aba0de84ae32b926dea10f60
notliad <dailton.rcf@gmail.com>
2023-01-21 03:16:18 -0300

    add static image for reference to home page background

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background-image: url(${"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"});
+`;
 
 export const ComponentsContainer = styled.div`
   margin: 0 20%;
   height: 100vh;
   align-items: center;
   justify-content: center;
-  background-color: #ff33;
 `;
